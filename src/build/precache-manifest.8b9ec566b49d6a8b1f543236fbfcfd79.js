90b52e199f29d636c335a82b2964739204bc040a
tooktee <tooktee.website@gmail.com>
2019-11-11 01:46:41 +0700

    first commit

---
+++ src/build/precache-manifest.8b9ec566b49d6a8b1f543236fbfcfd79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df60c30aeb489306a41a29de5046b2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa46f368cb1b2ca2eb5",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "ac2c3a8fe417895b6efd",
+    "url": "/static/js/2.061bcbc6.chunk.js"
+  },
+  {
+    "revision": "0aa46f368cb1b2ca2eb5",
+    "url": "/static/js/main.0ee7701d.chunk.js"
+  },
+  {
+    "revision": "dddd6d9f4658bad2d843",
+    "url": "/static/js/runtime-main.6353ac23.js"
+  }
+]);
